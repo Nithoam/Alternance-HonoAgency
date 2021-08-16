82883074120210a6a516580067eed24b8e3fa7fd
Nithoam <nithoam.nicolas@gmail.com>
2021-08-16 20:19:41 +0200

    exo 2 finished

---
+++ exercice-02/index.php
@@ -0,0 +1,21 @@
+<?php
+function shortcodeSocialMedia($settings) {
+  extract(
+    shortcode_atts(
+      array(
+        'content' => '', // url or pathway for the picture
+        'link' => '', 
+        'title' => '', // Link's Title and picture's alt
+        'rel' => '' // the relationship between our website and the link
+      ),
+      $settings
+    )
+  );
+
+$tagLink = '<a href="'.$link.'" title="'.$title.'" rel="'.$rel.'">'.'<img src="'.$content.'" alt="'.$title.'">'.'</a>';
+
+return $tagLink;
+};
+add_shortcodeSocialMedia('show_social_network', 'link');
+
+?>
